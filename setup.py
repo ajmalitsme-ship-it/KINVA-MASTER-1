6b99f22d628ce489746702ddb148c093f48ea66f
BosshuBots <ajmalitsme@gmail.com>
2026-04-19 22:28:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mx-language",
+    name="mx",
     version="1.0.0",
     author="MX Language Team",
     description="MX Programming Language with runprint as main function",
