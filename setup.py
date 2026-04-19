7622f666f9c8ac1ee3411e61d42c4a0c2dcaf7b6
BosshuBots <ajmalitsme@gmail.com>
2026-04-19 21:53:53 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mx-language",
+    version="1.0.0",
+    author="MX Language Team",
+    description="MX Programming Language with runprint as main function",
+    packages=find_packages(),
+    py_modules=['mx_interpreter'],
+    entry_points={
+        'console_scripts': [
+            'mx=mx_interpreter:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
